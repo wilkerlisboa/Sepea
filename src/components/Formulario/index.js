cbff1e5e51fd55686ec61a3e5e86e21121513f1e
leon paolo <leon.paolo.br.lp@gmail.com>
2024-06-18 19:52:37 -0300

    add validation CPF

--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -4,24 +4,49 @@ import { z } from "zod";
 import { Button, Form } from "react-bootstrap";
 import mask from "../utils/maascaraCpfCnpj";
 
+const validarCPF = (cpf) => {
+  cpf = cpf.replace(/[^\d]+/g, '');
+  if (cpf.length !== 11) return false;
+  if (/^(\d)\1+$/.test(cpf)) return false;
+  
+  let soma = 0;
+  let resto;
+  
+  for (let i = 1; i <= 9; i++) {
+      soma += parseInt(cpf.substring(i-1, i)) * (11 - i);
+  }
+  resto = (soma * 10) % 11;
+  
+  if (resto === 10 || resto === 11) resto = 0;
+  if (resto !== parseInt(cpf.substring(9, 10))) return false;
+  
+  soma = 0;
+  for (let i = 1; i <= 10; i++) {
+      soma += parseInt(cpf.substring(i-1, i)) * (12 - i);
+  }
+  resto = (soma * 10) % 11;
+  
+  if (resto === 10 || resto === 11) resto = 0;
+  if (resto !== parseInt(cpf.substring(10, 11))) return false;
+  
+  return true;
+};
+
 const formSchema = z.object({
   nome_orgao: z.string().min(1, "Nome do órgão público é obrigatório"),
   nome_gestor: z.string().min(1, "Nome do gestor é obrigatório"),
-  cpf_gestor: z
-    .string()
-    .min(14, "CPF deve ter no mínimo 11 caracteres")
-    .max(14, "CPF deve ter no máximo 11 caracteres"),
+  cpf_gestor: z.string().refine((value) => validarCPF(value) , {
+      message: "CPF inválido.",
+  }),
   nome_tecnico: z.string().min(1, "Nome do técnico é obrigatório"),
-  cpf_tecnico: z
-    .string()
-    .min(14, "CPF deve ter no mínimo 11 caracteres")
-    .max(14, "CPF deve ter no máximo 11 caracteres"),
+  cpf_tecnico: z.string().refine((value) => validarCPF(value) , {
+      message: "CPF inválido.",
+  }),
   contato_tecnico: z.string().min(1, "Contato do técnico é obrigatório"),
   nome_beneficiario: z.string().min(1, "Nome do beneficiário é obrigatório"),
-  cpf_beneficiario: z
-    .string()
-    .min(14, "CPF deve ter no mínimo 11 caracteres")
-    .max(14, "CPF deve ter no máximo 11 caracteres"),
+  cpf_beneficiario: z.string().refine((value) => validarCPF(value) , {
+      message: "CPF inválido.",
+  }),
   contato_beneficiario: z
     .string()
     .min(1, "Contato do beneficiário é obrigatório"),
